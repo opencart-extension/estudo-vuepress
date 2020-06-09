6cab5550487bb266f692f258421abfae43574877
Fuz Naval <naval.psr@gmail.com>
2020-06-09 12:26:29 -0300

    feat: create component social network

---
+++ .vuepress/theme/components/SocialNetwork.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <a href="#" class="small-social">
+      <i class="pixicon-facebook3 big-icon-50 pix-slight-white"></i>
+    </a>
+    <a href="#" class="small-social">
+      <i class="pixicon-twitter4 big-icon-50 pix-slight-white"></i>
+    </a>
+    <a href="#" class="small-social">
+      <i class="pixicon-instagram4 big-icon-50 pix-slight-white"></i>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SocialNetwork'
+}
+</script>
