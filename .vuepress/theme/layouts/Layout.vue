b9268279794154b198aa1eadd5c30266cbaedba4
Fuz Naval <naval.psr@gmail.com>
2020-06-09 12:00:51 -0300

    feat: create layout base

---
+++ .vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 col-xs-12">
+        <div class="pix-content pix-padding-bottom-30">
+          <Content />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout'
+}
+</script>
+
+<style>
+
+</style>
